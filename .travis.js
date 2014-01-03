707116a3b6ac966b3c7d2eb211378a4800fd60f0
Shahar Kedar <shahar@bigpanda.io>
2014-01-03 07:54:46 +0200

    Testing myself

---
+++ .travis.js
@@ -0,0 +1,6 @@
+language: node_js
+node_js:
+  - "0.10"
+notifications:
+  webhooks:
+    - http://1ab369d8.ngrok.com/travis/hook
---
+++ test/unit/test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('this is', function() {
+  it('a test', function() {
+    expect(true).to.be.true;
+  })
+})
